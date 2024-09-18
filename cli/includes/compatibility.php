ad9b467cfd9b7e05d8782ca7e96121ece2111618
JirakLu <jirak.lukas@gmail.com>
2024-09-18 11:07:52 +0200

    feat: edit the compatibility check

--- cli/includes/compatibility.php
+++ cli/includes/compatibility.php
@@ -8,22 +8,16 @@ if (php_sapi_name() !== 'cli') {
 /**
  * Check the system's compatibility with Valet.
  */
-$inTestingEnvironment = strpos($_SERVER['SCRIPT_NAME'], 'phpunit') !== false;
+$inTestingEnvironment = str_contains($_SERVER['SCRIPT_NAME'], 'phpunit');
 
-if (PHP_OS !== 'Darwin' && ! $inTestingEnvironment) {
-    echo 'Valet only supports the Mac operating system.'.PHP_EOL;
+if (PHP_OS !== 'Linux' && ! $inTestingEnvironment) {
+    echo 'Valet only supports the Linux operating system.'.PHP_EOL;
 
     exit(1);
 }
 
 if (version_compare(PHP_VERSION, '8.0', '<')) {
     echo 'Valet requires PHP 8.0 or later.';
-
-    exit(1);
-}
-
-if (exec('which brew') == '' && ! $inTestingEnvironment) {
-    echo 'Valet requires Homebrew to be installed on your Mac.';
 
     exit(1);
 }
